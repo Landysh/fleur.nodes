72b28d37ccff5ef4376fe8ce5341f5daa9914776
Aaron Hart <aaron.hart@gmail.com>
2016-02-15 16:56:55 +0100

    Initial Commit
    thats just it

---
+++ src/org/JOSC/FCSReaderNodeFactory.java
@@ -0,0 +1,58 @@
+package org.JOSC;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeFactory</code> for the "FCSReader" Node.
+ * It will do stuff	
+ *
+ * @author Landysh
+ */
+public class FCSReaderNodeFactory 
+        extends NodeFactory<FCSReaderNodeModel> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public FCSReaderNodeModel createNodeModel() {
+        return new FCSReaderNodeModel();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNrNodeViews() {
+        return 1;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeView<FCSReaderNodeModel> createNodeView(final int viewIndex,
+            final FCSReaderNodeModel nodeModel) {
+        return new FCSReaderNodeView(nodeModel);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasDialog() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return new FCSReaderNodeDialog();
+    }
+
+}
+
