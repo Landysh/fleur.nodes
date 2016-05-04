d5d75dd3c02233a49cd941e12ae7111c3871c56c
Aaron Hart <aaron.hart@gmail.com>
2016-05-04 16:04:16 +0200

    Introduce base parameters concept

--- src/io/landysh/inflor/java/core/SingletsModel.java
+++ src/io/landysh/inflor/java/core/SingletsModel.java
@@ -6,13 +6,15 @@ import java.util.Hashtable;
 
 public class SingletsModel {
 	
-	String[] 					 			modelInputColumnNames 	= null;
+	String[]		  			 			modelInputColumnNames 	= null;
+	String[]								modelBaseParameters		= null;
 	String[][] 					 			modelInputVectorPairs 	= null;
 	Hashtable<String, ArrayList<Double>> 	modelVectors 			= null;
 	
 	public SingletsModel(String[] columnNames) throws Exception {
+		modelBaseParameters = findBaseParameters(columnNames);
 		modelInputColumnNames = findUsableColumnNames(columnNames);
-		modelInputVectorPairs  = createVectorPairs(modelInputColumnNames);
+//		modelInputVectorPairs  = createVectorPairs(modelInputColumnNames);
 		if (modelInputColumnNames == null){
 			Exception e = new Exception("No valid pulse shape columns identified. Use a trailing -A, -H, -W or height, width and area");
 			e.printStackTrace();
@@ -20,18 +22,54 @@ public class SingletsModel {
 		}
 	}
 	
+	private String[] findBaseParameters(String[] inputColumnNames) {
+		ArrayList<String> baseParameters = new ArrayList<String>();
+		for(String s: inputColumnNames){
+			if (hasShapeInfo(s)){
+				String baseParameter = parseBaseParameter(s);
+				if (!baseParameters.contains(baseParameter)){
+					baseParameters.add(baseParameter);
+				}
+			} else {
+				if (!baseParameters.contains(s)){
+					baseParameters.add(s);
+				}
+			}
+		}
+		return (String[]) baseParameters.toArray();
+	}
+
+	private String parseBaseParameter(String s) {
+		String[] parts = s.toLowerCase().split("-");
+		
+		return null;
+	}
+
+	private boolean hasShapeInfo(String s) {
+		String[] parts = s.toLowerCase().split("-");
+		if (parts[parts.length]=="a" || parts[parts.length]=="area"){
+			return true;
+		} else if (parts[parts.length]=="h" || parts[parts.length]=="height"){
+			return true;
+		} else if (parts[parts.length]=="w" || parts[parts.length]=="width"){
+			return true;
+		} else{
+		return false;
+		}
+	}
+
 	public String[] getModelColumnNames() {
 		return modelInputColumnNames;
 	}
 	
-	private String[][] createVectorPairs(String[] inputNames) {
+	private String[][] createVectorPairs(ArrayList<String> modelInputColumnNames2) {
 		/**
 		 *  This method re-organizes any potentially interesting column names into groups for subsequent iteration.
 		 */
 		int masterParameterCount = 0;
-		for (int i=0;i<inputNames.length;i++){
-			// TODO the interesting part.
-		}
+//		for (int i=0;i<modelInputColumnNames2.length;i++){
+//			// TODO the interesting part.
+//		}
 		String[][] vectorSets = new String[masterParameterCount][3];
 		
 		return vectorSets;
@@ -55,7 +93,8 @@ public class SingletsModel {
 	}
 
 	public String[] getSingletsParameters() {
-			return modelInputColumnNames;
+		/** @return the list of usable parameters for singlets filtering.*/	
+		return modelInputColumnNames;
 	}
 
 	public void setData(Hashtable<String, ArrayList<Double>> data) {
