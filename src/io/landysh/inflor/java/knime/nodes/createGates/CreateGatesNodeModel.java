a489bb02303311ce036be20f6c71d8f739b9b2c4
Aaron Hart <aaron.hart@gmail.com>
2016-08-11 15:26:09 -0700

    new node: create gates
    initial commit, not working yet (compilation errors!)

---
+++ src/io/landysh/inflor/java/knime/nodes/createGates/CreateGatesNodeModel.java
@@ -0,0 +1,149 @@
+package io.landysh.inflor.java.knime.nodes.createGates;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.knime.core.data.DataCell;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataRow;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.RowKey;
+import org.knime.core.data.container.DataContainer;
+import org.knime.core.data.def.DefaultRow;
+import org.knime.core.data.def.DoubleCell;
+import org.knime.core.data.def.IntCell;
+import org.knime.core.data.def.StringCell;
+import org.knime.core.node.BufferedDataContainer;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeLogger;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+
+
+/**
+ * This is the model implementation of CreateGates.
+ * 
+ *
+ * @author 
+ */
+public class CreateGatesNodeModel extends NodeModel {
+    
+    // the logger instance
+    private static final NodeLogger logger = NodeLogger
+            .getLogger(CreateGatesNodeModel.class);
+        
+    GatingModelNodeSettings m_settings;
+    
+
+    /**
+     * Constructor for the node model.
+     */
+    protected CreateGatesNodeModel() {
+    
+        // TODO one incoming port and one outgoing port is assumed
+        super(1, 1);
+        
+        m_settings = new GatingModelNodeSettings();
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
+            final ExecutionContext exec) throws Exception {
+
+        // TODO do something here
+        logger.info("Executing Create Gates");
+        inData[0].spliterator();
+        DataContainer container;
+		// once we are done, we close the container and return its table
+        container.close();
+        BufferedDataTable out = container.getTable();
+        return new BufferedDataTable[]{out};
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void reset() {
+        // TODO Code executed on reset.
+        // Models build during execute are cleared here.
+        // Also data handled in load/saveInternals will be erased here.
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
+            throws InvalidSettingsException {
+        
+        // TODO: check if user settings are available, fit to the incoming
+        // table structure, and the incoming types are feasible for the node
+        // to execute. If the node can execute in its current state return
+        // the spec of its output data table(s) (if you can, otherwise an array
+        // with null elements), or throw an exception with a useful user message
+
+        return new DataTableSpec[]{inSpecs[0]};
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveSettingsTo(final NodeSettingsWO settings) {
+        
+        m_settings.save(settings);
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
+            throws InvalidSettingsException {
+            
+        m_settings.load(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void validateSettings(final NodeSettingsRO settings)
+            throws InvalidSettingsException {
+        m_settings.validate(settings);
+
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadInternals(final File internDir,
+            final ExecutionMonitor exec) throws IOException,
+            CanceledExecutionException {
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveInternals(final File internDir,
+            final ExecutionMonitor exec) throws IOException,
+            CanceledExecutionException {
+    }
+
+}
+//EOF
