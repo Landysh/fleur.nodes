f4676f24d6e8015db45738d95d9baf8fbb2dab35
Aaron Hart <aaron.hart@gmail.com>
2016-09-16 19:09:15 +0200

    added real data to lineage view test.

--- src/io/landysh/inflor/tests/ui/LineageViewUITest.java
+++ src/io/landysh/inflor/tests/ui/LineageViewUITest.java
@@ -1,68 +1,92 @@
 package io.landysh.inflor.tests.ui;
 
+import java.awt.Dimension;
 import java.util.Hashtable;
 
-import javax.swing.JTree;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
+import javax.swing.UIDefaults;
+import javax.swing.UIManager;
 
 import org.jfree.ui.ApplicationFrame;
 
-import io.landysh.inflor.java.core.plots.PlotSpec;
-import io.landysh.inflor.java.knime.nodes.createGates.ui.LineageAnalysisPanel;
+import io.landysh.inflor.java.core.dataStructures.ColumnStore;
+import io.landysh.inflor.java.core.fcs.FCSFileReader;
+import io.landysh.inflor.java.core.plots.ChartSpec;
+import io.landysh.inflor.java.core.plots.PlotTypes;
+import io.landysh.inflor.java.core.transforms.LogrithmicDisplayTransform;
+import io.landysh.inflor.java.knime.nodes.createGates.ui.CellLineageTree;
 
 @SuppressWarnings("serial")
 public class LineageViewUITest extends ApplicationFrame {
 	   public LineageViewUITest(String title) throws Exception {
 		super(title);
 		
-		Hashtable<String, PlotSpec> testSpecs = new Hashtable<String, PlotSpec>();
+		Hashtable<String, ChartSpec> testSpecs = new Hashtable<String, ChartSpec>();
 		
-		PlotSpec spec1 = new PlotSpec();
-		PlotSpec spec2 = new PlotSpec();
-		PlotSpec spec3 = new PlotSpec();
+		String logiclePath = "src/io/landysh/inflor/tests/extData/logicle-example.fcs";
+		final FCSFileReader reader = new FCSFileReader(logiclePath, false);
+		reader.readData();
+		final ColumnStore dataStore = reader.getColumnStore();
 		
-		spec2.setParent(spec1.getUUID());
+		ChartSpec ly = new ChartSpec();
+		ly.setPlotType(PlotTypes.Contour);
+		ly.setDomainAxisName("SSC-W");
+		ly.setRangeAxisName("SSC-A");
+		ly.setxBinCount(256);
+		ly.setyBinCount(256);
+		ly.setxMin(0);
+		ly.setyMin(1000);
+		ly.setxMax(262144);
+		ly.setyMax(262144);
+		
+		ChartSpec ly2 = new ChartSpec();
+		ly2.setPlotType(PlotTypes.Contour);
+		ly2.setDomainAxisName("FSC-A");
+		ly2.setRangeAxisName("SSC-A");
+		ly2.setRangeTransform(new LogrithmicDisplayTransform(1000, 262144));
+		ly2.setxBinCount(1024);
+		ly2.setyBinCount(1024);
+		ly2.setxMin(0);
+		ly2.setyMin(1000);
+		ly2.setxMax(262144);
+		ly2.setyMax(262144);
+		ly2.setParent(ly.UUID);
+		
+		ChartSpec ly3 = new ChartSpec();
+		ly3.setPlotType(PlotTypes.Scatter);
+		ly3.setDomainAxisName("FSC-A");
+		ly3.setRangeAxisName("SSC-A");
+		ly3.setxBinCount(1024);
+		ly3.setyBinCount(1024);
+		ly3.setxMin(0);
+		ly3.setyMin(1000);
+		ly3.setxMax(262144);
+		ly3.setyMax(262144); 
+		ly3.setParent(ly.UUID);
 
-		testSpecs.put(spec1.uuid, spec1);
-		testSpecs.put(spec2.uuid, spec2);
-		testSpecs.put(spec3.uuid, spec3);
-		
-		
-		//
-		String s1 = "Foo";
-		String s2 = "FooBar";
-		String s3 = "Bar";
-		
-		DefaultMutableTreeNode root = new DefaultMutableTreeNode("patoot");
-
-		DefaultMutableTreeNode n1 = new DefaultMutableTreeNode(s1);
-		DefaultMutableTreeNode n2 = new DefaultMutableTreeNode(s2);
-		DefaultMutableTreeNode n3 = new DefaultMutableTreeNode(s3);
-		
-		DefaultTreeModel model = new DefaultTreeModel(root);
-		model.insertNodeInto(n1, root, 0);
-		model.insertNodeInto(n3, root, 1);
-		model.insertNodeInto(n2, n1, 0);
-
-		
-		
-		JTree tree = new JTree(model);
-		tree.setRootVisible(false);
-
-		
 		
 
-		LineageAnalysisPanel testPanel = new LineageAnalysisPanel(testSpecs);
-		testPanel.updateLayout();
-		testPanel.setRootVisible(true);
+		testSpecs.put(ly.UUID, ly);
+		testSpecs.put(ly2.UUID, ly2);
+		testSpecs.put(ly3.UUID, ly3);
 
-		this.getContentPane().add(tree);
+		
+		UIDefaults defaults = UIManager.getDefaults();
+		Integer oldValue = (int) defaults.get("Tree.leftChildIndent");
+		defaults.put("Tree.leftChildIndent" , new Integer(110) );
+
+		CellLineageTree testPanel = new CellLineageTree();
+		testPanel.updateLayout(testSpecs, dataStore);
+		testPanel.setRootVisible(false);
+
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.getContentPane().add(testPanel);
+		defaults.put("Tree.leftChildIndent" , oldValue );
 	}
 
 	public static void main(String[] args) throws Exception {
 		LineageViewUITest test = new LineageViewUITest("ContourPlotTest");
 			  test.pack();
+			  test.setSize(new Dimension(400, 700));
 		      test.setVisible(true);
 	}	   
 }
