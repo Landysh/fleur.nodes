82b0601f31fede38888f21eae4f9606adb35e08c
Aaron Hart <aaron.hart@gmail.com>
2016-10-05 19:38:46 +0200

    ColumnStore now using Collections API.
    I didn't know that it was a thing, now i do.  Lambda seems pretty nice.

--- src/io/landysh/inflor/java/core/dataStructures/ColumnStore.java
+++ src/io/landysh/inflor/java/core/dataStructures/ColumnStore.java
@@ -4,66 +4,37 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Hashtable;
+import java.util.TreeMap;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 
-import io.landysh.inflor.java.core.proto.AnnotatedVectorMessage.AnnotatedVectorsProto;
-import io.landysh.inflor.java.core.proto.AnnotatedVectorMessage.AnnotatedVectorsProto.Keyword;
-import io.landysh.inflor.java.core.proto.AnnotatedVectorMessage.AnnotatedVectorsProto.Vector;
+import io.landysh.inflor.java.core.proto.FCSFrameProto.Message;
+import io.landysh.inflor.java.core.proto.FCSFrameProto.Message.Keyword;
+import io.landysh.inflor.java.core.utils.FCSUtils;
+import io.landysh.inflor.java.core.proto.FCSFrameProto.Message.Dimension;
 
-public class ColumnStore {
+
+//don't use the default serializer, there is a protobuf spec.
+@SuppressWarnings("serial")
+public class ColumnStore extends DomainObject{
 
 	private static final String DEFAULT_PREFFERED_NAME_KEYWORD = "$FIL";
 
-	public static ColumnStore load(byte[] bytes) throws InvalidProtocolBufferException {
-		final AnnotatedVectorsProto message = AnnotatedVectorsProto.parseFrom(bytes);
+	
 
-		// Load the keywords
-		final Hashtable<String, String> keywords = new Hashtable<String, String>();
-		for (int i = 0; i < message.getKeywordsCount(); i++) {
-			final Keyword keyword = message.getKeywords(i);
-			final String key = keyword.getKey();
-			final String value = keyword.getValue();
-			keywords.put(key, value);
-		}
-		// Load the vectors
-		final int rowCount = message.getVectors(0).getArrayCount();
-		final ColumnStore columnStore = new ColumnStore(keywords, rowCount);
-		final int columnCount = message.getVectorsCount();
-		final String[] vectorNames = new String[columnCount];
-		for (int j = 0; j < columnCount; j++) {
-			final Vector vector = message.getVectors(j);
-			final String key = vector.getName();
-			vectorNames[j] = key;
-			final double[] values = new double[vector.getArrayCount()];
-			for (int i = 0; i < values.length; i++) {
-				values[i] = vector.getArray(i);
-			}
-			columnStore.addColumn(key, values);
-		}
-		columnStore.setPreferredName(columnStore.getKeywordValue(DEFAULT_PREFFERED_NAME_KEYWORD));
-		return columnStore;
-	}
-
-	public static ColumnStore load(FileInputStream input) throws Exception {
-		final byte[] buffer = new byte[input.available()];
-		input.read(buffer);
-		final ColumnStore columnStore = ColumnStore.load(buffer);
-		return columnStore;
-	}
-
-	// file details
-	public String UUID;
+	private TreeMap<String, FCSDimension> columnData;
 
 	private Hashtable<String, String> keywords;
-	private Hashtable<String, FCSDimension> columnData;
+	private String preferredName;
 
 	// data properties
 	private Integer rowCount = -1;
-	private String preferredName;
+	// file details
+	public String UUID;
 
 	// minimal constructor, use with .load()
 	public ColumnStore() {
+		super(null);
 	}
 
 	/**
@@ -74,24 +45,23 @@ public class ColumnStore {
 	 *            header but may be added to later.
 	 */
 	public ColumnStore(Hashtable<String, String> keywords, int rowCount) {
+		this(null, keywords, rowCount);
+	}
+	
+	public ColumnStore(String priorUUID, Hashtable<String, String> keywords, int rowCount) {
+		super(priorUUID);
 		this.keywords = keywords;
-		columnData = new Hashtable<String, FCSDimension>();
+		columnData = new TreeMap<String, FCSDimension>();
 		this.rowCount = rowCount;
 		preferredName = getKeywordValue(DEFAULT_PREFFERED_NAME_KEYWORD);
 	}
 
-	public void addColumn(String name, double[] data) {
-		if (rowCount == data.length) {
-			final FCSDimension newDimension = new FCSDimension(name, data, new Hashtable<String, String>());
-			columnData.put(name, newDimension);
+	public void addColumn(String name, FCSDimension newDim) {
+		if (rowCount == newDim.getSize()) {
+			columnData.put(name, newDim);
 		} else {
 			throw new IllegalStateException("New dimension does not match frame size: " + rowCount.toString());
 		}
-	}
-
-	public double[] getDimensionData(String xName) {
-		double[] data = columnData.get(xName).getData();
-		return data;
 	}
 
 	public int getColumnCount() {
@@ -105,8 +75,13 @@ public class ColumnStore {
 		return columnNames;
 	}
 
-	public Hashtable<String, FCSDimension> getData() {
+	public TreeMap<String, FCSDimension> getData() {
 		return columnData;
+	}
+
+	public double[] getDimensionData(String displayName) {
+		FCSDimension matchingDimension = FCSUtils.findCompatibleDimension(this.getData(), displayName);
+		return matchingDimension.getData();
 	}
 
 	public Hashtable<String, String> getKeywords() {
@@ -153,44 +128,56 @@ public class ColumnStore {
 
 	public byte[] save() {
 		// create the builder
-		final AnnotatedVectorsProto.Builder messageBuilder = AnnotatedVectorsProto.newBuilder();
+		final Message.Builder messageBuilder = Message.newBuilder();
 
 		// Should add UUID field here.
+		messageBuilder.setEventCount(this.rowCount);
+		messageBuilder.setId(ID);
 
-		// add the vector names.
-		for (final String s : getColumnNames()) {
-			messageBuilder.addVectorNames(s);
+		// add the dimension names.
+		for (final String name : getColumnNames()) {
+			messageBuilder.addDimNames(name);
 		}
 
 		// add the keywords.
 		for (final String s : keywords.keySet()) {
 			final String key = s;
 			final String value = keywords.get(s);
-			final AnnotatedVectorsProto.Keyword.Builder keyBuilder = AnnotatedVectorsProto.Keyword.newBuilder();
+			final Message.Keyword.Builder keyBuilder = Message.Keyword.newBuilder();
 			keyBuilder.setKey(key);
 			keyBuilder.setValue(value);
-			final AnnotatedVectorsProto.Keyword keyword = keyBuilder.build();
-			messageBuilder.addKeywords(keyword);
+			final Message.Keyword newKeyword = keyBuilder.build();
+			messageBuilder.addKeyword(newKeyword);
 		}
-		// add the data.
+		// add the FCS Dimensions.
 		final Integer size = getColumnNames().length;
 		for (int i = 0; i < size; i++) {
-			final AnnotatedVectorsProto.Vector.Builder vectorBuilder = AnnotatedVectorsProto.Vector.newBuilder();
+			final Message.Dimension.Builder dimBuilder = Message.Dimension.newBuilder();
+			
+			//Add required information.
 			final String name = getColumnNames()[i];
-			// Raw data
+			FCSDimension fcsdim = columnData.get(name);
+			dimBuilder.setIndex(fcsdim.getIndex());
+			dimBuilder.setPnn(fcsdim.getShortName());
+			dimBuilder.setPns(fcsdim.getStainName());
+			dimBuilder.setPneF1(fcsdim.getPNEF1());
+			dimBuilder.setPneF2(fcsdim.getPNEF2());
+			dimBuilder.setPnr(fcsdim.getRange());
+			dimBuilder.setCompRef(fcsdim.getCompRef());
+			dimBuilder.setId(fcsdim.ID);
+			// Add the numeric data
 			final double[] rawArray = columnData.get(name).getData();
-			vectorBuilder.setName(name);
-			for (final double element : rawArray) {
-				vectorBuilder.addArray(element);
+			for (final double value : rawArray) {
+				dimBuilder.addData(value);
 			}
-			final AnnotatedVectorsProto.Vector v = vectorBuilder.build();
-			messageBuilder.addVectors(v);
+			final Message.Dimension dim = dimBuilder.build();
+			messageBuilder.addDimension(dim);
 		}
 
 		// build the message
-		final AnnotatedVectorsProto AVSProto = messageBuilder.build();
-		final byte[] message = AVSProto.toByteArray();
-		return message;
+		final Message buffer = messageBuilder.build();
+		final byte[] bytes = buffer.toByteArray();
+		return bytes;
 	}
 
 	public void save(FileOutputStream out) throws IOException {
@@ -198,8 +185,46 @@ public class ColumnStore {
 		out.write(message);
 		out.flush();
 	}
+	
+	public static ColumnStore load(byte[] bytes) throws InvalidProtocolBufferException {
+		final Message loadedMessage = Message.parseFrom(bytes);
 
-	public void setData(Hashtable<String, FCSDimension> allData) {
+		// Load the keywords
+		final Hashtable<String, String> keywords = new Hashtable<String, String>();
+		for (int i = 0; i < loadedMessage.getKeywordCount(); i++) {
+			final Keyword keyword = loadedMessage.getKeyword(i);
+			final String key = keyword.getKey();
+			final String value = keyword.getValue();
+			keywords.put(key, value);
+		}
+		// Load the vectors
+		final int rowCount = loadedMessage.getEventCount();
+		final ColumnStore columnStore = new ColumnStore(loadedMessage.getId(), keywords, rowCount);
+		final int dimCount = loadedMessage.getDimensionCount();
+		final String[] dimen = new String[dimCount];
+		for (int j = 0; j < dimCount; j++) {
+			Dimension dim = loadedMessage.getDimension(j);
+			String priorUUID = dim.getId();
+			dimen[j] = priorUUID;
+			final FCSDimension values = new FCSDimension(priorUUID, columnStore.getRowCount(), dim.getIndex(), dim.getPnn(), 
+					dim.getPns(), dim.getPneF1(), dim.getPneF2(), dim.getPnr(), dim.getCompRef());
+			for (int i = 0; i < values.getSize(); i++) {
+				values.getData()[i] = dim.getData(i);
+			}
+			columnStore.addColumn(priorUUID, values);
+		}
+		columnStore.setPreferredName(columnStore.getKeywordValue(DEFAULT_PREFFERED_NAME_KEYWORD));
+		return columnStore;
+	}
+
+	public static ColumnStore load(FileInputStream input) throws Exception {
+		final byte[] buffer = new byte[input.available()];
+		input.read(buffer);
+		final ColumnStore columnStore = ColumnStore.load(buffer);
+		return columnStore;
+	}
+
+	public void setData(TreeMap<String, FCSDimension> allData) {
 		columnData = allData;
 		rowCount = allData.get(getColumnNames()[0]).getData().length;
 	}
