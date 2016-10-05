dd741112c80f8194de67b092ade467e5c4d23118
Aaron Hart <aaron.hart@gmail.com>
2016-10-05 19:41:28 +0200

    Rewrite of column store to FCSDimension.
    Needed to go specifically to the FCS dimension as a mechanism for
    transporting domain logic.

--- src/io/landysh/inflor/java/core/dataStructures/FCSDimension.java
+++ src/io/landysh/inflor/java/core/dataStructures/FCSDimension.java
@@ -1,34 +1,118 @@
 package io.landysh.inflor.java.core.dataStructures;
 
-import java.util.Hashtable;
+//Default serialization not used on suspicion of performance issues. We should measure this...
+@SuppressWarnings("serial")
+public class FCSDimension extends DomainObject implements Comparable<FCSDimension>{
+	
+	//eg. the n in PnN
+	private int parameterIndex;
+	
+	//$PnE Amplification type 
+	private double ampTypef1;
+	private double ampTypef2;
+	
+	//$PnN Short name 
+	private String shortName;
+	
+	//$PnS Stain name 
+	private String stainName;
+	
+	boolean compRef;
+	
+	//$PnR Range
+	private double range;
+	
+	private double[] data;
 
-public class FCSDimension {
-
-	private final String dimensionName;
-
-	private final double[] data;
-	private Hashtable<String, String> keywords;
-
-	public FCSDimension(String name, double[] data, Hashtable<String, String> keywords) {
-		dimensionName = name;
-		this.data = data;
-		keywords = new Hashtable<String, String>();
+	public FCSDimension(int size, int index, String pnn, 
+			String pns, double pneF1, double pneF2, double pnr, boolean wasComped) {
+		this(null,size,index,pnn,pns,pneF1,pneF2,pnr,wasComped);
+	}
+	
+	public FCSDimension(String priorUUID, int size, int index, String pnn, 
+			String pns, double pneF1, double pneF2, double pnr, boolean wasComped) {
+		super(priorUUID);
+		parameterIndex = index;
+		shortName = pnn;
+		stainName = pns;
+		ampTypef1 = pneF1;
+		ampTypef2 = pneF2;
+		compRef = wasComped;
+		this.data = new double[size] ;
 	}
 
 	public double[] getData() {
 		return data;
 	}
 
-	public String getKeyword(String name) {
-		return keywords.get(name);
-	}
-
 	public int getSize() {
 		return this.data.length;
 	}
 
-	public String getName() {
-		return dimensionName;
+	public String getDisplayName(){
+		if (compRef==true){
+			if (stainName!=null){
+				return "[" + shortName +": " + stainName + "]"; 
+			} else {
+				return "[" + shortName + "]";
+			}
+		} else {
+			if (stainName!=null){
+				return shortName +": " + stainName; 
+			} else {
+				return shortName;
+			}
+		}
+	}
+
+	@Override
+	public int compareTo(FCSDimension other) {
+		int result = 0;	
+		if (parameterIndex<parameterIndex){
+			result-=1;
+		} else if (parameterIndex>parameterIndex){
+			result+=1;
+		} else {
+			result = 0;
+		}
+	return result;
+	}
+
+	public int getIndex() {
+		return this.parameterIndex;
+	}
+
+	public String getShortName() {
+		return this.shortName;
+	}
+
+	public String getStainName() {
+		return this.stainName;
+	}
+
+	public double getPNEF1() {
+		return this.ampTypef1;
+	}
+
+	public double getPNEF2() {
+		return this.ampTypef2;
+	}
+
+	public double getRange() {
+		return this.range;
+	}
+
+	public boolean getCompRef() {
+		return this.compRef;
+	}
+
+	public void setData(double[] newData) {
+		this.data = newData;
+	}
+	
+	@Override
+	public String toString(){
+		return this.getDisplayName();
 	}
 	
 }
