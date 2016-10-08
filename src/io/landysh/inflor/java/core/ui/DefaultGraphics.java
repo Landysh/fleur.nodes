1c7d336f77679dbef8cadec9028de3e3fe83a7cc
Aaron Hart <aaron.hart@gmail.com>
2016-10-08 11:44:26 +0200

    Initial commit
    A place to store application specific look and feel.

---
+++ src/io/landysh/inflor/java/core/ui/DefaultGraphics.java
@@ -0,0 +1,10 @@
+package io.landysh.inflor.java.core.ui;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+
+public final class DefaultGraphics {
+	public static final BasicStroke DEFAULT_STROKE 		= new BasicStroke();
+	public static final BasicStroke SELECTED_STROKE 	= new BasicStroke(2);
+	public static final Color 		DEFAULT_GATE_COLOR	= Color.getHSBColor(50/360, 1, (float) 0.99);
+}
