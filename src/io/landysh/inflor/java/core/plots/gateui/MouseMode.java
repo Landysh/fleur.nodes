1ea60d02f175c9f43351911221be14878eede52e
Aaron Hart <aaron.hart@gmail.com>
2016-10-08 11:41:50 +0200

    Initial commit
    Toolbar to change mouse selection modes.

---
+++ src/io/landysh/inflor/java/core/plots/gateui/MouseMode.java
@@ -0,0 +1,5 @@
+package io.landysh.inflor.java.core.plots.gateui;
+
+public enum MouseMode {
+	DEFAULT, RECTANGLE
+}
---
+++ src/io/landysh/inflor/java/core/ui/GateCreationToolBar.java
@@ -0,0 +1,76 @@
+package io.landysh.inflor.java.core.ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+
+import io.landysh.inflor.java.core.gatingML.gates.rangeGate.FCSChartPanel;
+import io.landysh.inflor.java.core.gatingML.gates.rangeGate.RectangleGateListener;
+
+@SuppressWarnings("serial")
+public class GateCreationToolBar extends JToolBar {
+    private static final String TOOLBAR_TITLE = "Mouse Mode";
+	/**
+     * This toolbar is responsible for managing the active listener (gate drawing/zooming mode)
+     * on a JFreeChart ChartPanel. 
+     */
+	
+	private ArrayList<JButton> cursorButtons;
+	MouseListener activeListener;
+	private FCSChartPanel panel;
+	
+	public GateCreationToolBar(){
+		super(TOOLBAR_TITLE);
+		
+		cursorButtons = new ArrayList<JButton>();
+		
+		JButton zoomButton = createSelectButton();
+	    JButton rectGateButton = createRectGateButton();
+	    
+	    cursorButtons = new ArrayList<JButton>();
+	    cursorButtons.add(zoomButton);
+	    cursorButtons.add(rectGateButton);    
+	    cursorButtons.forEach(button-> this.add(button));
+	}
+
+	private JButton createSelectButton() {
+		JButton button = new JButton("Select");
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				cursorButtons.forEach(button -> button.setEnabled(true));
+				((JButton)arg0.getSource()).setEnabled(false);
+				panel.removeMouseListener(activeListener);
+				panel.removeMouseMotionListener((MouseMotionListener) activeListener);
+			}
+		});	
+		return button;
+	}
+
+	private JButton createRectGateButton() {
+	    JButton button = new JButton("RectGate");
+	    button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				//Set button states.
+				cursorButtons.forEach(button -> button.setEnabled(true));
+				((JButton)arg0.getSource()).setEnabled(false);
+				
+				//Remove current listener.
+				panel.removeMouseListener(activeListener);
+				panel.removeMouseMotionListener((MouseMotionListener) activeListener);
+				
+				//Create new rect gate listener.
+				activeListener = new RectangleGateListener(panel);
+				panel.addMouseListener(activeListener);
+				panel.addMouseMotionListener((MouseMotionListener) activeListener);
+			}
+		});
+		return button;
+	}
+}
