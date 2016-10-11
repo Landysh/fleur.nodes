2c10a25dd2a104c72b8f27347b693e3125357968
Aaron Hart <aaron.hart@gmail.com>
2016-10-11 10:45:06 +0200

    Updated default colors

--- src/io/landysh/inflor/java/core/ui/DefaultGraphics.java
+++ src/io/landysh/inflor/java/core/ui/DefaultGraphics.java
@@ -6,5 +6,6 @@ import java.awt.Color;
 public final class DefaultGraphics {
 	public static final BasicStroke DEFAULT_STROKE 		= new BasicStroke();
 	public static final BasicStroke SELECTED_STROKE 	= new BasicStroke(2);
-	public static final Color 		DEFAULT_GATE_COLOR	= Color.getHSBColor(50/360, 1, (float) 0.99);
+	public static final Color 		DEFAULT_GATE_COLOR	= Color.BLACK;
+	public static final Color 		SELECTED_GATE_COLOR = Color.DARK_GRAY;
 }
