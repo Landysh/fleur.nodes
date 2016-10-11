3cc451d11134fe2157e493b7f0ef9c45eee19cab
Aaron Hart <aaron.hart@gmail.com>
2016-10-11 10:43:59 +0200

    Initial commit

---
+++ src/io/landysh/inflor/java/core/utils/ChartUtils.java
@@ -0,0 +1,29 @@
+package io.landysh.inflor.java.core.utils;
+
+import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.plot.XYPlot;
+
+import io.landysh.inflor.java.core.plots.FCSChartPanel;
+import io.landysh.inflor.java.core.plots.gateui.RectangleGateAnnotation;
+
+public class ChartUtils {
+	public static Point2D getPlotCoordinates(MouseEvent e, ChartPanel panel){
+        Point2D p = panel.translateScreenToJava2D(e.getPoint());
+        Rectangle2D plotArea = panel.getScreenDataArea();
+        XYPlot plot = panel.getChart().getXYPlot();
+        double x = plot.getDomainAxis().java2DToValue(p.getX(), plotArea, plot.getDomainAxisEdge());
+        double y = plot.getRangeAxis().java2DToValue(p.getY(), plotArea, plot.getRangeAxisEdge());
+        Point2D vertex = new Point2D.Double(x, y);
+        return vertex;
+    }
+
+	public static void updateRectangleAnnotation(RectangleGateAnnotation oldAnn,
+			RectangleGateAnnotation newAnn, FCSChartPanel panel) {
+		// TODO Auto-generated method stub
+		
+	}
+}
