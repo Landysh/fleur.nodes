8d779dc0e84e8de2681999ec914b36835e83f929
Aaron Hart <aaron.hart@gmail.com>
2016-10-22 22:52:45 +0200

    basic histogram functionality

--- src/io/landysh/inflor/java/core/dataStructures/Histogram1D.java
+++ src/io/landysh/inflor/java/core/dataStructures/Histogram1D.java
@@ -2,13 +2,29 @@ package io.landysh.inflor.java.core.dataStructures;
 
 public class Histogram1D {
 
-	public Histogram1D(double[] data, int size) {
-		// TODO Auto-generated constructor stub
-	}
+	private double[] x;
+	private double[] y;
 
-	public void findPeaks() {
-		// TODO Auto-generated method stub
+	public Histogram1D(double[] data, double min, double max, int binCount) {
 		
-	}
+	double deltaX = (max-min)/binCount;
 
+	x = new double[binCount];
+	for(int i=0;i<binCount;i++){
+		x[i] = i*deltaX;
+	}
+	y = new double[binCount];
+	for(int i=0;i<binCount;i++){y[i] = 0;}
+	for(int i=0;i<data.length;i++){
+		int bin = (int) (data[i]/deltaX);
+		if (bin>=y.length){
+			i=i;
+		}
+		y[bin]++;
+		}
+	}
+	
+	public double[][] getData(){
+		return new double[][] {x,y};
+	}
 }
