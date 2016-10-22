11827422b9ffca2b7c7828181af7e39c3a7544d5
Aaron Hart <aaron.hart@gmail.com>
2016-10-22 22:54:26 +0200

    Added min and max scale values to transform to provide ranges for easier axis generation in JFreeChart.

--- src/io/landysh/inflor/java/core/transforms/AbstractTransform.java
+++ src/io/landysh/inflor/java/core/transforms/AbstractTransform.java
@@ -16,7 +16,9 @@ public abstract class AbstractTransform implements Serializable, Cloneable {
 	abstract public double[] transform(double[] rawData);
 	abstract public double transform(double value);
 	abstract public double inverse(double value);
-
+	abstract public double getMinValue();
+	abstract public double getMaxValue();
+	
 	public TransformType getType() {
 		return this.type;
 	}
