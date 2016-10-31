c1656fc91e7241fe141abaeb2c27c1aad6a3e9f5
Aaron Hart <aaron.hart@gmail.com>
2016-10-31 11:19:11 +0100

    Improved tick refresh

--- src/io/landysh/inflor/java/core/plots/LogicleNumberAxis.java
+++ src/io/landysh/inflor/java/core/plots/LogicleNumberAxis.java
@@ -32,11 +32,30 @@ public class LogicleNumberAxis extends NumberAxis {
 	@Override
 	public List<NumberTick> refreshTicks(Graphics2D g2, AxisState state, Rectangle2D dataArea, RectangleEdge edge){
 		List<NumberTick> ticks = new ArrayList<NumberTick>();
-		for (double d: logicle.getAxisValues()){
-			double td = logicle.transform(d);
-			String label = this.getNumberFormatOverride().format(td);
-			NumberTick tick = new NumberTick(td, label, TextAnchor.CENTER, TextAnchor.CENTER, 0);
-			ticks.add(tick);
+		double[] suggestedTicks = logicle.getAxisValues();
+		if (suggestedTicks[0] >=0){
+			double tick1Value = logicle.getMinTranformedValue();
+			double tick2Value = logicle.transform(Math.abs(logicle.getMinRawValue()));
+
+			String label1 = this.getNumberFormatOverride().format(tick1Value);
+			String label2 = this.getNumberFormatOverride().format(tick2Value);
+
+			ticks.add(new NumberTick(tick1Value, label1, TextAnchor.TOP_CENTER, TextAnchor.TOP_CENTER, 0));
+			ticks.add(new NumberTick(tick2Value, label2, TextAnchor.TOP_CENTER, TextAnchor.TOP_CENTER, 0));
+		}
+		
+		for (double value: suggestedTicks){
+			double td = logicle.transform(value);
+			double tickDangerRatio = 0;
+			if (ticks.size()>=2){
+				double firstPositiveTick = logicle.inverse(ticks.get(1).getValue());
+				tickDangerRatio = (value/firstPositiveTick);
+			}
+			if (tickDangerRatio<0.5||tickDangerRatio>2){
+				String label = this.getNumberFormatOverride().format(td);
+				NumberTick tick = new NumberTick(td, label, TextAnchor.TOP_CENTER, TextAnchor.TOP_CENTER, 0);
+				ticks.add(tick);
+			}
 		}
 		return ticks;
 	}
