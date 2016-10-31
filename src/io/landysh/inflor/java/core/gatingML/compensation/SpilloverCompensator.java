e40d01e599d1a55e8f441e0e47d353bc0014b723
Aaron Hart <aaron.hart@gmail.com>
2016-10-31 11:17:16 +0100

    Added function to compensate whole dataFrame

--- src/io/landysh/inflor/java/core/gatingML/compensation/SpilloverCompensator.java
+++ src/io/landysh/inflor/java/core/gatingML/compensation/SpilloverCompensator.java
@@ -4,56 +4,58 @@ import static org.ejml.ops.CommonOps.invert;
 import static org.ejml.ops.CommonOps.mult;
 
 import java.util.HashMap;
-import org.ejml.data.DenseMatrix64F;
 
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.ops.CommonOps;
+
+import io.landysh.inflor.java.core.dataStructures.FCSFrame;
+import io.landysh.inflor.java.core.dataStructures.DomainObject;
+import io.landysh.inflor.java.core.dataStructures.FCSDimension;
 import io.landysh.inflor.java.core.utils.FCSUtils;
 
-public class SpilloverCompensator {
+@SuppressWarnings("serial")
+public class SpilloverCompensator extends DomainObject{
 
 	// Compensation details
-	public String[] compParameters;
+	public  String[]  compParameters;
 	private Integer[] compParameterMap;
 	private DenseMatrix64F compMatrix;
 	private double[][] rawMatrix;
 
-	public SpilloverCompensator(HashMap<String, String> keywords) {
+	public SpilloverCompensator(HashMap<String, String> keywords) {this(keywords, null);}
+	public SpilloverCompensator(HashMap<String, String> keywords, String priorUUID) {
+		super(priorUUID);
 		if (keywords.containsKey("$SPILL") || keywords.containsKey("SPILL")) {
 			rawMatrix = parseSpillover(keywords);
 			compMatrix = new DenseMatrix64F(rawMatrix);
 			invert(compMatrix);
 		}
+		validate();
+	}
+
+	private void validate() {
+		if(compParameters.length<=1){
+			throw new RuntimeException("Invalid compensation matrix");
+		}
 	}
 
 	// TODO: Unit test
 	public double[] compensateRow(double[] FCSRow) {
-		double[] compedRow = null;
-		if (compParameters != null) {
-			compedRow = new double[compParameters.length];
-			final double[] unCompedRow = new double[compParameters.length];
-			for (int i = 0; i < compParameters.length; i++) {
-				final int index = compParameterMap[i];
-				unCompedRow[i] = FCSRow[index];
-			}
-			final DenseMatrix64F unCompedVector = new DenseMatrix64F(new double[][] { unCompedRow });
-			final DenseMatrix64F c = new DenseMatrix64F(new double[][] { unCompedRow });
-			mult(unCompedVector, compMatrix, c);
-			compedRow = c.data;
+		double[] compedRow = new double[compParameters.length];
+		final double[] unCompedRow = new double[compParameters.length];
+		for (int i = 0; i < compParameters.length; i++) {
+			final int index = compParameterMap[i];
+			unCompedRow[i] = FCSRow[index];
 		}
-		if (compedRow != null) {
-			return compedRow;
-		} else {
-			final Exception e = new Exception("Comped array is null, this should not happen.");
-			e.printStackTrace();
-			return null;
-		}
+		final DenseMatrix64F unCompedVector = new DenseMatrix64F(new double[][] { unCompedRow });
+		final DenseMatrix64F c = 			  new DenseMatrix64F(new double[][] { unCompedRow });
+		mult(unCompedVector, compMatrix, c);
+		compedRow = c.data;
+		return compedRow;
 	}
 
 	public String[] getCompParameterNames() {
 		return compParameters;
-	}
-
-	public double[][] getMatrix() {
-		return rawMatrix;
 	}
 
 	private double[][] parseSpillover(HashMap<String, String> keywords) {
@@ -99,4 +101,41 @@ public class SpilloverCompensator {
 			return null;
 		}
 	}
+
+	public FCSFrame compensateFCSFrame(FCSFrame dataFrame) {
+		double[][] X = new double[compParameters.length][dataFrame.getRowCount()];
+		
+		for (int i=0;i<compParameters.length;i++){
+			FCSDimension dimension = FCSUtils.findCompatibleDimension(dataFrame, compParameters[i]);
+			if (dimension == null){
+			  for (String s: compParameters){
+			    System.out.println(s);
+			  }
+			  RuntimeException e = new RuntimeException("DataFrame does not contain matching parameters: " + compParameters[i]);
+			  e.printStackTrace();
+			  throw e;
+			}
+			X[i] = dimension.getData();
+		}
+		DenseMatrix64F XT = new DenseMatrix64F(X);
+		CommonOps.transpose(XT);
+		//not sure about mutability.
+		mult(XT.copy(), compMatrix, XT);
+		CommonOps.transpose(XT);
+		
+		X = new double[compParameters.length][dataFrame.getRowCount()];
+		for (int i=0;i<compParameters.length;i++){
+			for (int j=0;j<dataFrame.getRowCount();j++){
+				double newVal = XT.get(i,j);
+				X[i][j] = 	newVal;
+			}
+		}
+		for (int i=0;i<compParameters.length;i++){
+			FCSDimension dimension = FCSUtils.findCompatibleDimension(dataFrame, compParameters[i]);
+			dimension.setData(X[i]);
+		}
+		dataFrame.setCompRef(this.ID);
+		return dataFrame;
+	}
+	public double[][] getMatrix() {return rawMatrix;}
 }
