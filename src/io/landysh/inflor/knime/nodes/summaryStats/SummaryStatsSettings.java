3fec5fd6210f9968635d8bcc2a87cc86ceae5dc5
Aaron Hart <aaron.hart@gmail.com>
2016-11-28 11:29:35 +0100

    New node: Summary statistics

--- src/io/landysh/inflor/knime/nodes/summaryStats/SummaryStatsSettings.java
+++ src/io/landysh/inflor/knime/nodes/summaryStats/SummaryStatsSettings.java
@@ -1,57 +1,65 @@
 package io.landysh.inflor.knime.nodes.summaryStats;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
-import org.knime.core.node.defaultnodesettings.SettingsModelString;
-import org.knime.core.node.defaultnodesettings.SettingsModelStringArray;
+
+import io.landysh.inflor.java.knime.core.NodeUtilities;
 
 public class SummaryStatsSettings {
 
-  static final String KEY_RESPONSE_MARKERS = "ResponseMarkers";
-  static final String[] RESPONSE_MARKERS_DEFAULT = null;
-  static final String SELECTED_STATS_KEY = "SelectedStats";
-
-  static final String[] SELECTED_STATS_DEFAULT = null;
-  // Selected column from input table upon which to do the discrimination.
-  static final String FCS_COLUMN_KEY = "FCSColumn";
-  static final String FCS_COLUMN_DEFAULT = null;
-
-  private final SettingsModelStringArray m_responseMarkers =
-      new SettingsModelStringArray(KEY_RESPONSE_MARKERS, RESPONSE_MARKERS_DEFAULT);
-  private final SettingsModelStringArray m_selectedStats =
-      new SettingsModelStringArray(SELECTED_STATS_KEY, SELECTED_STATS_DEFAULT);
-  private final SettingsModelString m_FCSColumn =
-      new SettingsModelString(FCS_COLUMN_KEY, FCS_COLUMN_DEFAULT);
-
-  public SettingsModelStringArray getResponseMarkersModel() {
-    return m_responseMarkers;
+  private static final String SELECTED_COLUMN_KEY = "Selected Column";
+  private String modelSelectedColumn;
+  private List<StatSpec> stats; 
+  
+  private static final String STATS_KEY = "Statistic Definitions";
+  
+  public SummaryStatsSettings(){
+    stats = new ArrayList<StatSpec>();
   }
-
-  public SettingsModelString getSelectedColumnSettingsModel() {
-    return m_FCSColumn;
-  }
-
-  public SettingsModelStringArray getSelectedStatsModel() {
-    return m_selectedStats;
+  
+  public void save(NodeSettingsWO settings) {
+    settings.addString(SELECTED_COLUMN_KEY, modelSelectedColumn);
+    NodeUtilities.saveSerializable(settings, STATS_KEY, (Serializable) stats);
   }
 
   public void load(NodeSettingsRO settings) throws InvalidSettingsException {
-    m_selectedStats.loadSettingsFrom(settings);
-    m_responseMarkers.loadSettingsFrom(settings);
-    m_FCSColumn.loadSettingsFrom(settings);
+    modelSelectedColumn = settings.getString(SELECTED_COLUMN_KEY);
+    List<Serializable> serialStats = NodeUtilities.loadList(settings, STATS_KEY);
+    stats = serialStats
+        .stream()
+        .map(ser -> (StatSpec) ser)
+        .collect(Collectors.toList());
+  }
+  
+  public String getSelectedColumn() {
+    return modelSelectedColumn;
+  }
+  
+  public void setSelectedColumn(String newValue) {
+    modelSelectedColumn = newValue;
   }
 
-  public void save(NodeSettingsWO settings) {
-    m_selectedStats.saveSettingsTo(settings);
-    m_responseMarkers.saveSettingsTo(settings);
-    m_FCSColumn.saveSettingsTo(settings);
+  public void validate(NodeSettingsRO settings) {
+    //TODO
   }
 
-  public void validate(NodeSettingsRO settings) throws InvalidSettingsException {
-    m_selectedStats.validateSettings(settings);
-    m_responseMarkers.validateSettings(settings);
-    m_FCSColumn.validateSettings(settings);
+  public void addStatSpec(StatSpec statSpec) {
+    if (statSpec!=null&&!stats.contains(statSpec)){
+      stats.add(statSpec);
+    }
+  }
+
+   public List<StatSpec> getStatSpecs() {
+    return stats;
+  }
+
+  public void removeStat(StatSpec spec) {
+    stats.remove(spec);
   }
 }
-// EOF
