3f1d5f62afe60ecd1a1778d85ef7f9ab2489b4a6
Aaron Hart <aaron.hart@gmail.com>
2016-12-15 12:08:18 +0100

    added copyright claim + license

--- src/io/landysh/inflor/main/core/ui/CellLineageTree.java
+++ src/io/landysh/inflor/main/core/ui/CellLineageTree.java
@@ -1,8 +1,27 @@
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright 2016 by Aaron Hart
+ *  Email: Aaron.Hart@gmail.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ * ---------------------------------------------------------------------
+ *
+ * Created on December 14, 2016 by Aaron Hart
+ */
 package io.landysh.inflor.main.core.ui;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.swing.JTree;
