d31869e3fd7ff1da7cf8ee115c46a985dbd29d8c
Aaron Hart <aaron.hart@gmail.com>
2016-12-15 12:08:47 +0100

    fixed transform serialization bug

--- src/io/landysh/inflor/main/core/dataStructures/FCSFrame.java
+++ src/io/landysh/inflor/main/core/dataStructures/FCSFrame.java
@@ -1,3 +1,23 @@
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright 2016 by Aaron Hart
+ *  Email: Aaron.Hart@gmail.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ * ---------------------------------------------------------------------
+ *
+ * Created on December 14, 2016 by Aaron Hart
+ */
 package io.landysh.inflor.main.core.dataStructures;
 
 import java.io.FileInputStream;
@@ -229,8 +249,8 @@ public class FCSFrame extends DomainObject implements Comparable<String> {
     } else if (transform instanceof BoundDisplayTransform) {
       tBuilder.setTransformType(TransformType.BOUNDARY.toString());
       BoundDisplayTransform boundaryTransform = (BoundDisplayTransform) transform;
-      tBuilder.setBoundMin(boundaryTransform.getMinRawValue());
-      tBuilder.setLogMax(boundaryTransform.getMaxRawValue());
+      tBuilder.setBoundMin(boundaryTransform.getMinTranformedValue());
+      tBuilder.setBoundMax(boundaryTransform.getMaxTransformedValue());
     }
     tBuilder.setId(transform.getID());
     return tBuilder;
