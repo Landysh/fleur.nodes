ec9eb8a345547ca95bb7816704b4dcff80437711
Aaron Hart <aaron.hart@gmail.com>
2016-12-19 18:52:11 +0100

    added compensation tests

--- src/io/landysh/inflor/tests/unit/InflorTestSuite.java
+++ src/io/landysh/inflor/tests/unit/InflorTestSuite.java
@@ -5,6 +5,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({FCSFileReaderTest.class, EventFrameTest.class})
+@SuiteClasses({FCSFileReaderTest.class, 
+               EventFrameTest.class,
+               CompensatorTest.class
+               })
 public class InflorTestSuite {
 }
