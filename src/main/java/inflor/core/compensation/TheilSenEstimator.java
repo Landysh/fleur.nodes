8de0438c347b5c8eb250603822bdf1871a29390d
Aaron Hart <aaron.hart@gmail.com>
2017-01-21 10:57:43 +0100

    Added Hi/Low with draft ssm calculation

--- src/main/java/inflor/core/compensation/TheilSenEstimator.java
+++ src/main/java/inflor/core/compensation/TheilSenEstimator.java
@@ -1,5 +1,6 @@
 package main.java.inflor.core.compensation;
 
+import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 import org.apache.commons.math3.stat.descriptive.rank.Median;
 
 public class TheilSenEstimator {
@@ -15,18 +16,13 @@ public class TheilSenEstimator {
         }
       }
       Median median = new Median();
-      double mValue = median.evaluate(ms);
-      if (mValue > 0.3&& mValue<1)
-        System.out.print(mValue);
-      return mValue;
+      return median.evaluate(ms);
     } else {
       throw new IllegalArgumentException("x and y must not be null and must have a length > 0");
     }
   }
 
-  public static double evaluate2(String outName, String name, double[] x1,
-      double[] x2, double[] y1,
-      double[] y2) {
+  public static double evaluate2(double[] x1, double[] x2, double[] y1, double[] y2) {
     double[] spills = new double[x1.length*x2.length];
     if (x1.length==y1.length&&x2.length==y2.length){
       for (int i=0;i<x1.length;i++){
@@ -36,9 +32,22 @@ public class TheilSenEstimator {
       }
     }
     Median median = new Median();
-    double mValue = median.evaluate(spills);
-    if (mValue > 0.3&& mValue<1)
-      System.out.print(mValue);
-    return mValue;
+    return median.evaluate(spills);
+  }
+  
+  public static double[] evaluateHiLo(double[] x1, double[] x2, double[] y1, double[] y2) {    
+    
+    StandardDeviation dev = new StandardDeviation();
+    Median med = new Median();
+    double brightX = med.evaluate(x2);
+    double brightY = med.evaluate(y2);
+    double dimX = med.evaluate(x1);
+    double dimY = med.evaluate(y1);
+    
+    double dx = brightX-dimX;
+    double dy = brightY - dimY;
+    double ds = dev.evaluate(y2)-dev.evaluate(y1);
+    
+    return new double[]{dy/dx, ds/Math.pow(dx,0.5)};
   }
 }
