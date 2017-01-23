75ee43e4b95226c108817f0d3d5187771e5a2eb3
Aaron Hart <aaron.hart@gmail.com>
2017-01-23 17:03:27 +0100

    Recangle gate is gone, updating tests.

--- src/tests/java/inflor/ui/LineageViewFrame.java
+++ src/tests/java/inflor/ui/LineageViewFrame.java
@@ -1,6 +1,7 @@
 package tests.java.inflor.ui;
 
 import java.awt.Dimension;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,17 +13,22 @@ import org.jfree.ui.ApplicationFrame;
 import main.java.inflor.core.data.FCSFrame;
 import main.java.inflor.core.fcs.FCSFileReader;
 import main.java.inflor.core.gates.Hierarchical;
-import main.java.inflor.core.gates.RectangleGate;
+import main.java.inflor.core.gates.RangeGate;
 import main.java.inflor.core.plots.ChartSpec;
 import main.java.inflor.core.plots.PlotTypes;
 import main.java.inflor.core.ui.CellLineageTree;
 
 @SuppressWarnings("serial")
 public class LineageViewFrame extends ApplicationFrame {
-  public LineageViewFrame(String title) throws Exception {
+  private static final String TREE_LEFT_CHILD_INDENT = "Tree.leftChildIndent";
+  private static final String DIM3 = "FSC-A";
+  private static final String DIM2 = "SSC-A";
+  private static final String DIM1 = "SSC-W";
+
+  public LineageViewFrame(String title) throws IOException {
     super(title);
 
-    List<Hierarchical> testSpecs = new ArrayList<Hierarchical>();
+    List<Hierarchical> testSpecs = new ArrayList<>();
 
     String logiclePath = "src/io/landysh/inflor/tests/extData/logicle-example.fcs";
     final FCSFileReader reader = new FCSFileReader(logiclePath);
@@ -31,22 +37,23 @@ public class LineageViewFrame extends ApplicationFrame {
 
     ChartSpec ly = new ChartSpec();
     ly.setPlotType(PlotTypes.DENSITY);
-    ly.setDomainAxisName("SSC-W");
-    ly.setRangeAxisName("SSC-A");
+    ly.setDomainAxisName(DIM1);
+    ly.setRangeAxisName(DIM2);
 
     ChartSpec ly2 = new ChartSpec();
     ly2.setPlotType(PlotTypes.DENSITY);
-    ly2.setDomainAxisName("FSC-A");
-    ly2.setRangeAxisName("SSC-A");
+    ly2.setDomainAxisName(DIM3);
+    ly2.setRangeAxisName(DIM2);
 
 
     ChartSpec ly3 = new ChartSpec();
     ly3.setPlotType(PlotTypes.DENSITY);
-    ly3.setDomainAxisName("FSC-A");
-    ly3.setRangeAxisName("SSC-A");
+    ly3.setDomainAxisName(DIM3);
+    ly3.setRangeAxisName(DIM2);
     ly3.setParentID(ly.getID());
     
-    RectangleGate g1 = new RectangleGate("LY", "FSC-A", 25000, 100000, "SSC-A", 36000, 200000, "gate");
+    RangeGate g1 = new RangeGate("LY", new String[]{DIM3, DIM2}, new double[]{25000, 100000}, new double[]{36000,200000}, "gate");
+        
     g1.setParentID(dataStore.getID());
     ly.setParentID("gate");
 
@@ -57,15 +64,15 @@ public class LineageViewFrame extends ApplicationFrame {
 
 
     UIDefaults defaults = UIManager.getDefaults();
-    Integer oldValue = (int) defaults.get("Tree.leftChildIndent");
-    defaults.put("Tree.leftChildIndent", new Integer(110));
+    Integer oldValue = (int) defaults.get(TREE_LEFT_CHILD_INDENT);
+    defaults.put(TREE_LEFT_CHILD_INDENT, 110);
 
     CellLineageTree testPanel = new CellLineageTree(dataStore,testSpecs);
     testPanel.setRootVisible(true);
 
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     this.getContentPane().add(testPanel);
-    defaults.put("Tree.leftChildIndent", oldValue);
+    defaults.put(TREE_LEFT_CHILD_INDENT, oldValue);
   }
 
   public static void main(String[] args) throws Exception {
